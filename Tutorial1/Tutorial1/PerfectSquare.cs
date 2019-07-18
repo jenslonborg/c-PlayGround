918bf49b13378d23f1483623d198d4b2016412fb
Jens Lønborg <jenslonborg@gmail.com>
2019-07-18 21:06:57 +0200

    Some basic c# syntax

---
+++ Tutorial1/Tutorial1/PerfectSquare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PerfectSquareApplication
+{
+    class PerfectSquare
+    {
+        // member variables
+        // none
+        public bool IsPerfect(double input)
+        {
+            if (Math.Sqrt(input) == Math.Round(Math.Sqrt(input)))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RectangleApplication;
+using PerfectSquareApplication;
+
+namespace Tutorial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello World
+            Console.WriteLine("Hello World!");
+            //Basics
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            r.SetDetails(1.1, 3.1);
+            r.Display();
+            //Some fun
+            PerfectSquare perf = new PerfectSquare();
+            for (double n = 1; n <= 100000; n++)
+            {
+                double candidate = n * n + 45;
+                if (perf.IsPerfect(candidate))
+                {
+                    Console.WriteLine("{0} gives a perfect square. It is {1} -> {2}", n, candidate, Math.Sqrt(candidate));
+                }
+            }
+            Console.WriteLine("Found all perfect Squares");
+            Console.ReadKey();
+        }
+    }
+}
